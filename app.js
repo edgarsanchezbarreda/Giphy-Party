b7dd7b288da15bd5d3c9c02254cc318e7b3d1f88
Edgar Sanchez <edgar@Edgars-Air.attlocal.net>
2022-01-20 20:22:49 -0800

    added img to div

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const giphyBox = document.querySelector('#giphyBox');
 const searchBtn = document.querySelector('#searchBtn');
 const searchBox = document.querySelector('#searchBox');
+const contentBox = document.querySelector('#contentBox');
 
 
-async function getGif(){
+
+async function getGif(query){
+   let queryVal = searchBox.value;
    const response = await axios.get("http://api.giphy.com/v1/gifs/search", {params: {
-   q: 'puppy', 
+   q: queryVal, 
    api_key: 'SNRTYSwTiVeMmEczl4NlImbMyIiOO1yi'}});
-//    console.log(response.data.data);
-   const imgUrl = response.data.data;
-   
 
    const newImg = document.createElement('img');
-   newImg.src = imgUrl;
-   searchBtn.addEventListener('click', function(e){
-    e.preventDefault();
-    giphyBox.append(newImg);
-})
+   let resultsLength = response.data.data.length;
+   let imgIndex = Math.floor(Math.random() * resultsLength);
+   
+   newImg.src = response.data.data[imgIndex].images.original.url;
+   contentBox.addEventListener('click', async function(e){
+   e.preventDefault();
+   giphyBox.append(newImg);
+    })
+   
 }
 getGif();
