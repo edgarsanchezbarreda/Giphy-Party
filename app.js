d2aa9a2f52d11265de863a0eb4ced77179cfc2e3
Edgar Sanchez <edgar@Edgars-Air.attlocal.net>
2022-01-20 19:46:24 -0800

    found search query URLs

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const giphyBox = document.querySelector('#giphyBox');
 const searchBtn = document.querySelector('#searchBtn');
+const searchBox = document.querySelector('#searchBox');
+
 
 async function getGif(){
-   const response = await axios.get("http://api.giphy.com/v1/gifs/random", {params: {
+   const response = await axios.get("http://api.giphy.com/v1/gifs/search", {params: {
+   q: 'puppy', 
    api_key: 'SNRTYSwTiVeMmEczl4NlImbMyIiOO1yi'}});
-   const imgUrl = response.data.data.bitly_gif_url;
+//    console.log(response.data.data);
+   const imgUrl = response.data.data;
+   
 
    const newImg = document.createElement('img');
    newImg.src = imgUrl;
