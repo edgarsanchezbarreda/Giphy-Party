e4b65615ce27aef6c569fbc9aa0c113357617442
Edgar Sanchez <edgar@Edgars-Air.attlocal.net>
2022-01-20 18:26:23 -0800

    added an event listener

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-console.log("Let's get this party started!");
+const giphyBox = document.querySelector('#giphyBox');
+const searchBtn = document.querySelector('#searchBtn');
 
 async function getGif(){
-    const res = await axios.get('api.giphy.com/v1/gifs/random');
-    console.log(res);
+   const response = await axios.get("http://api.giphy.com/v1/gifs/random", {params: {
+   api_key: 'SNRTYSwTiVeMmEczl4NlImbMyIiOO1yi'}});
+   const imgUrl = response.data.data.bitly_gif_url;
+
+   const newImg = document.createElement('img');
+   newImg.src = imgUrl;
+   searchBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    giphyBox.append(newImg);
+})
 }
 getGif();
